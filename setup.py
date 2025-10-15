1c1f214b0190181de01989023e46d7605fcc752d
Lim-HG <103091082+Lim-HG@users.noreply.github.com>
2025-10-15 09:38:02 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='gradinglib',
-    version='1.0.1', # 새 버전
+    version='1.0.1', 
     packages=find_packages(),
-    package_data={'gradinglib': ['answers.json']},
+
+    package_data={
+        'gradinglib': [
+            'answers.json', 
+            '*.npy' 
+        ]
+    }, 
     description='Python assignment grading library',
     author='Lim Hyung Geun',
     author_email='mathlim@hoseo.cnehs.kr',
+    include_package_data=True 
 )
