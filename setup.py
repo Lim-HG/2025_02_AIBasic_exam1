339ab70aea8e9ad212fd320d587342db82cd74df
Lim-HG <103091082+Lim-HG@users.noreply.github.com>
2025-10-21 13:23:14 +0900

    Bump version from 1.0.1 to 1.0.2

--- setup.py
+++ setup.py
@@ -2,15 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='gradinglib',
-    version='1.0.1', 
+    version='1.0.2',                # ← 최신 버전
     packages=find_packages(),
-
     package_data={
-        'gradinglib': [
-            'answers.enc',
-            '*.npy'
-        ]
-    }, 
+        'gradinglib': ['answers.enc', '*.npy']
+    },
     description='Python assignment grading library',
     author='Lim Hyung Geun',
     author_email='mathlim@hoseo.cnehs.kr',
